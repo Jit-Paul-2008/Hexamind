d507492e4bdfa81bde0f4f8e9aa6a235882feffa
Jit-Paul-2008 <Jit-Paul-2008@users.noreply.github.com>
2026-04-06 12:13:03 +0000

    deploy: 64b3c59f8d28d6d2e0d9ba1de128ff21a0615dec

---
+++ _next/static/_Lm1dSSV1i2-qxTSVIlyl/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fworkspace\u002F[projectId]","\u002Fworkspace\u002F[projectId]\u002Fcase\u002F[caseId]","\u002Fworkspace\u002F[projectId]\u002Fcompare"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
